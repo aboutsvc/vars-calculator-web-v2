612ee8bc220b236e50799fd2fc9f1d71ad538382
Sunwoo Kim <sh.4215@gmail.com>
2025-01-18 22:37:50 +0900

    fix error

--- src/navigation.ts
+++ src/navigation.ts
@@ -18,57 +18,13 @@ export const headerData = {
   };
 
 export const footerData = {
-  // links: [
-  //   {
-  //     title: 'Product',
-  //     links: [
-  //       { text: 'Features', href: '#' },
-  //       { text: 'Security', href: '#' },
-  //       { text: 'Team', href: '#' },
-  //       { text: 'Enterprise', href: '#' },
-  //       { text: 'Customer stories', href: '#' },
-  //       { text: 'Pricing', href: '#' },
-  //       { text: 'Resources', href: '#' },
-  //     ],
-  //   },
-  //   {
-  //     title: 'Platform',
-  //     links: [
-  //       { text: 'Developer API', href: '#' },
-  //       { text: 'Partners', href: '#' },
-  //       { text: 'Atom', href: '#' },
-  //       { text: 'Electron', href: '#' },
-  //       { text: 'AstroWind Desktop', href: '#' },
-  //     ],
-  //   },
-  //   {
-  //     title: 'Support',
-  //     links: [
-  //       { text: 'Docs', href: '#' },
-  //       { text: 'Community Forum', href: '#' },
-  //       { text: 'Professional Services', href: '#' },
-  //       { text: 'Skills', href: '#' },
-  //       { text: 'Status', href: '#' },
-  //     ],
-  //   },
-  //   {
-  //     title: 'Company',
-  //     links: [
-  //       { text: 'About', href: '#' },
-  //       { text: 'Blog', href: '#' },
-  //       { text: 'Careers', href: '#' },
-  //       { text: 'Press', href: '#' },
-  //       { text: 'Inclusion', href: '#' },
-  //       { text: 'Social Impact', href: '#' },
-  //       { text: 'Shop', href: '#' },
-  //     ],
-  //   },
-  // ],
+  links: [
+  ],
   secondaryLinks: [
     { text: 'Terms', href: getPermalink('/terms') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
-  
+  socialLinks: [],
   footNote: `
     Made by <a class="text-blue-600 underline dark:text-muted" href="https://aboutsvc.com/"> AboutService</a> · All rights reserved.
   `,
